e15df39f2e0a004c43a3ed5594c27bb9276aef4e
mattew1 <ma15224915796@163.com>
2023-08-22 00:55:44 +0800

    commit

---
+++ docs/.vitepress/dist/assets/index.md.788645c5.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as s,k as a}from"./chunks/framework.f15d8f8c.js";const h=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"index.md","filePath":"index.md"}'),n={name:"index.md"},o=a("p",null,"hello vitepress",-1),r=[o];function c(d,i,_,l,p,m){return t(),s("div",null,r)}const x=e(n,[["render",c]]);export{h as __pageData,x as default};
